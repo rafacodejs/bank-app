93ed89d748399a00071d71d1b6256907270c5a35
Rafael Malpica <rafacodejs@gmail.com>
2022-09-19 10:39:51 -0400

    Finished components: Billing, Stats, Business

--- bank-app-page/src/components/Button.jsx
+++ bank-app-page/src/components/Button.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
-
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type='button'
+      className={`py-4 px-6 mt-6 rounded-xl bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none transition-all duration-500 ease-in-out transform hover:-translate-y-3  ${styles}}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export { Button };
