ac1331f2f7157c61c58f394c0956a3d548a6e905
Rafael Malpica <rafacodejs@gmail.com>
2022-09-19 15:03:52 -0400

    Finished components: Footer

--- bank-app-page/src/components/GetStarted.jsx
+++ bank-app-page/src/components/GetStarted.jsx
@@ -5,14 +5,14 @@ import '../index.css';
 
 const GetStarted = () => (
   <div
-    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer transition-all duration-500 ease-in-out transform hover:-translate-y-5`}
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer transition-all duration-500 ease-in-out transform hover:-translate-y-5 `}
   >
     <div
       className={`${styles.flexCenter} flex-col bg-primary  w-[100%] h-[100%] rounded-full`}
     >
-      <div className={`${styles.flexStart} flex-row`}>
+      <div className={`${styles.flexStart} flex-row `}>
         <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
-          <span className='text-gradient'>Get</span>
+          <span className='text-gradient '>Get</span>
         </p>
         <img
           src={arrowUp}
